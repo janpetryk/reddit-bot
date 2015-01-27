7a9890e6729a61c8a8f8f4cc655cf6e149714d1b
janpetryk <jasie3k@gmail.com>
2015-01-27 17:52:36 +0100

    Refactored runner

--- src/main/java/pl/jpetryk/redditbot/Runner.java
+++ src/main/java/pl/jpetryk/redditbot/Runner.java
@@ -8,11 +8,14 @@ import pl.jpetryk.redditbot.connectors.JrawRedditConnector;
 import pl.jpetryk.redditbot.connectors.RedditConnectorInterface;
 import pl.jpetryk.redditbot.connectors.Twitter4JConnector;
 import pl.jpetryk.redditbot.connectors.TwitterConnectorInterface;
+import pl.jpetryk.redditbot.exceptions.NetworkConnectionException;
+import pl.jpetryk.redditbot.exceptions.RedditApiException;
 import pl.jpetryk.redditbot.utils.CommentParser;
 import pl.jpetryk.redditbot.utils.PropertiesReader;
 import pl.jpetryk.redditbot.utils.ResponseCommentCreator;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -28,52 +31,72 @@ public class Runner {
 
     public static void main(String[] args) throws Exception {
 
-        Logger rootLogger = Logger.getRootLogger();
-        rootLogger.setLevel(Level.TRACE);
-        rootLogger.addAppender(new FileAppender(
-                new PatternLayout("%-6r [%p] %c - %m%n"), "bot.log"));
 
         PropertiesReader redditProperties = new PropertiesReader("resources/bot.properties");
-
-        RedditConnectorInterface redditConnector = new JrawRedditConnector.Builder()
-                .login(redditProperties.getProperty("reddit-login"))
-                .password(redditProperties.getProperty("reddit-password"))
-                .clientId(redditProperties.getProperty("reddit-client-id"))
-                .clientSecret(redditProperties.getProperty("reddit-client-secret"))
-                .userAgent(redditProperties.getProperty("reddit-useragent"))
-                .build();
-        logger.trace("created reddit connector");
-
         PropertiesReader twitterProperties = new PropertiesReader("resources/twitter.properties");
 
-        TwitterConnectorInterface twitterConnector = new Twitter4JConnector.Builder()
-                .apiKey(twitterProperties.getProperty("api-key"))
-                .apiSecret(twitterProperties.getProperty("api-secret"))
-                .accessToken(twitterProperties.getProperty("access-token"))
-                .accessTokenSecret(twitterProperties.getProperty("access-token-secret"))
-                .build();
+        configureLogger();
+
+        RedditConnectorInterface redditConnector = prepareRedditConnector(redditProperties);
+        logger.trace("created reddit connector");
+
+        TwitterConnectorInterface twitterConnector = prepareTwitterConnector(twitterProperties);
 
         logger.trace("created twitter connector");
 
         CommentParser parser = new CommentParser(twitterProperties.getProperty("twitter-status-regex"));
         logger.trace("created comment parser");
 
-        String responseTemplate = Files.toString(new File("resources/reply-template"), Charset.defaultCharset());
-        String footerTemplate = Files.toString(new File("resources/footer-template"), Charset.defaultCharset());
-        ResponseCommentCreator responseCommentCreator = new ResponseCommentCreator(responseTemplate, footerTemplate);
+        ResponseCommentCreator responseCommentCreator = prepareResponseCommentCreator();
         logger.trace("created response comment creator");
 
-        List<String> blacklist = new ArrayList<>(Arrays.asList(redditProperties.getProperty("blacklist").split(", ")));
-        blacklist.add(redditProperties.getProperty("reddit-login"));
-        AbstractRedditBot bot = new TweetsInCommentsBot(twitterConnector, redditConnector, parser,
-                redditProperties.getProperty("subreddits"), responseCommentCreator, blacklist);
+        AbstractRedditBot bot = prepareBot(redditProperties, redditConnector, twitterConnector, parser,
+                responseCommentCreator);
         logger.trace("created bot");
-
 
         while (true) {
             bot.run();
             Thread.sleep(Long.valueOf(redditProperties.getProperty("run-interval")));
         }
+    }
+
+    private static AbstractRedditBot prepareBot(PropertiesReader redditProperties, RedditConnectorInterface redditConnector, TwitterConnectorInterface twitterConnector, CommentParser parser, ResponseCommentCreator responseCommentCreator) {
+        List<String> blacklist = new ArrayList<>(Arrays.asList(redditProperties.getProperty("blacklist").split(", ")));
+        blacklist.add(redditProperties.getProperty("reddit-login"));
+        return new TweetsInCommentsBot(twitterConnector, redditConnector, parser,
+                redditProperties.getProperty("subreddits"), responseCommentCreator, blacklist);
+    }
+
+    private static ResponseCommentCreator prepareResponseCommentCreator() throws IOException {
+        String responseTemplate = Files.toString(new File("resources/reply-template"), Charset.defaultCharset());
+        String footerTemplate = Files.toString(new File("resources/footer-template"), Charset.defaultCharset());
+        return new ResponseCommentCreator(responseTemplate, footerTemplate);
+    }
+
+    private static Twitter4JConnector prepareTwitterConnector(PropertiesReader twitterProperties) {
+        return new Twitter4JConnector.Builder()
+                .apiKey(twitterProperties.getProperty("api-key"))
+                .apiSecret(twitterProperties.getProperty("api-secret"))
+                .accessToken(twitterProperties.getProperty("access-token"))
+                .accessTokenSecret(twitterProperties.getProperty("access-token-secret"))
+                .build();
+    }
+
+    private static JrawRedditConnector prepareRedditConnector(PropertiesReader redditProperties) throws NetworkConnectionException, RedditApiException {
+        return new JrawRedditConnector.Builder()
+                .login(redditProperties.getProperty("reddit-login"))
+                .password(redditProperties.getProperty("reddit-password"))
+                .clientId(redditProperties.getProperty("reddit-client-id"))
+                .clientSecret(redditProperties.getProperty("reddit-client-secret"))
+                .userAgent(redditProperties.getProperty("reddit-useragent"))
+                .build();
+    }
+
+    private static void configureLogger() throws IOException {
+        Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.WARN);
+        rootLogger.addAppender(new FileAppender(
+                new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} [%p] %c - %m%n"), "bot.log"));
     }
 
 
